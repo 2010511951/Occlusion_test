d0a1ad42e55ebf5a46369c9fe37dcc1adc33d818
2010511951 <2010511951@sjtu.edu.cn>
2019-03-15 18:26:38 +0800

    Update occlusion_show.py

--- occlusion_show.py
+++ occlusion_show.py
@@ -5,22 +5,16 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib import colors
 
-cdict = ['white','red']
-my_cmap = colors.ListedColormap(cdict,'indexed')
-
 occ_map=np.loadtxt('occlusion_map.txt',dtype=np.float64)
 
 plt.imshow(abs(occ_map), cmap=cm.hot)
 plt.colorbar()
 plt.show()
 
-'''
-scc_map_img = Image.fromarray(occ_map)'''
-
 occ_map_img = Image.fromarray(np.uint8(cm.hot(abs(occ_map))*255))
 
-tf_img = Image.open("dataset/sob_lateral.bmp")
-target_img = Image.open("dataset/Doc3_lateral.bmp")
+tf_img = Image.open("demo_data/sob_lateral.bmp")
+target_img = Image.open("demo_data/Doc3_lateral.bmp")
 ori_img = Image.new('RGB',(320,256),255)
 ori_img.paste(tf_img,(0,0))
 ori_img.paste(target_img,(0,128))
@@ -30,5 +24,5 @@ ori_img.paste(target_img,(0,128))
 
 blend_img = Image.blend(ori_img,occ_map_img.convert('RGB'),0.5)
 blend_img.show()
-blend_img.save("outcome/Sob-Doc3_blend.bmp")
+blend_img.save("Sob-Doc3_blend.bmp")
 
